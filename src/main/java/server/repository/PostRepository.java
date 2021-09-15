58599d7e3b22bab31ce53adae05943f869bea0c5
ruagegd <elena.kosheleva@raiffeisen.ru>
2021-09-15 20:02:49 +0300

    first commit

---
+++ src/main/java/server/repository/PostRepository.java
@@ -0,0 +1,60 @@
+package server.repository;
+
+import org.springframework.stereotype.Repository;
+import server.exception.NotFoundException;
+import server.model.Post;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+// Stub
+@Repository
+public class PostRepository {
+
+    private AtomicLong idCounter;
+    private Map<Long, Post> repository;
+
+    public PostRepository() {
+        repository = new ConcurrentHashMap<>();
+        idCounter = new AtomicLong(0);
+    }
+
+    public List<Post> all() {
+        return repository.values().stream().collect(Collectors.toList());
+    }
+
+    public Optional<Post> getById(long id) {
+        return repository.containsKey(id) ? Optional.of(repository.get(id))
+                                          : Optional.empty();
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            var id = idCounter.incrementAndGet();
+            post.setId(id);
+            repository.put(id, post);
+        } else {
+            if (repository.containsKey(post.getId())) {
+                repository.replace(post.getId(), post);
+            } else {
+                var id = idCounter.incrementAndGet();
+                ;
+                post.setId(id);
+                repository.put(id, post);
+            }
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        if (repository.containsKey(id)) {
+            repository.remove(id);
+        } else {
+            throw new NotFoundException();
+        }
+    }
+}
